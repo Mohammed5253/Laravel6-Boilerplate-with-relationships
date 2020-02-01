4c207edce71872b4d53209753b7ae2960209c15b
Mohammed5253 <ms.sabir120@gmail.com>
2020-02-01 15:56:22 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel6 Boilerplate</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,19 +81,16 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 6 Boilerplate
                 </div>
-
+               
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs/6.x/authentication#included-routing">Authentication using Laravel Auth</a>
+                    <a href="https://laravel.com/docs/6.x/eloquent-relationships">Elequent Relationships</a>
+                    <a href="https://laravel.com/docs/6.x/eloquent#introduction">Soft delete using Elequent Relationship</a>
+                    <a href="https://laravel.com/docs/6.x/telescope">Monitor performance using telescope </a>
                 </div>
+    
             </div>
         </div>
     </body>
